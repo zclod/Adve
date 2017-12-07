79aa95b8f08f6e513defca34d401443b8451d675
claudio <claudio.zan@outlook.com>
2017-12-07 15:06:13 +0100

    day 3 continua

--- day3/Main.hs
+++ day3/Main.hs
@@ -6,35 +6,45 @@ module Main where
 
 import qualified Prelude as P
 import           Prelude hiding (unlines, filter, interact, length, lines, min, max)
+import           Data.Monoid
 import           Data.Sequence (Seq, length, index, fromList)
 import           Data.Text (Text, unpack, unlines, lines, filter, pack)
 import           Data.Text.IO (interact)
 import           Text.Read(readMaybe)
-import           Data.Char
--- import           GHC.Generics
 import           Data.Attoparsec.Text
--- import           Control.Newtype
-import           Data.Semigroup
-import           Data.Fold.M1
-import           Control.Monad.Zip
-import           Control.Arrow
-import           Data.Profunctor
-import qualified Data.List.NonEmpty as NE
 
--- instance Newtype (Max a) a where
---     pack = Max
---     unpack (Max a) = a
+orderedsquare n = s1 <> s2 <> s3 <> s4
+  where
+    s1 = fromList $ P.zip (repeat 0) [0..(n-2)]
+    s2 = fromList $ P.zip [-1,-2..(-n+1)] (repeat (n-2))
+    s3 = fromList $ P.zip (repeat (-n+1)) [(n-3),n-4..(-1)]
+    s4 = fromList $ P.zip [(-n+2),-n+3..0] (repeat (-1))
 
--- instance Newtype (Min a) a where
---     pack = Min
---     unpack (Min a) = a
+translate (dx,dy) (x,y) = (x+dx, y+dy)
 
+findBasePoint n = (n, n-1)
 
--- il numero massimo al circolo n e x^2 dove x = (2*n+1)^2 (dispari) 1,3,5
--- il numero minimo al circolo n e (2*(n-1)+1)^2 + 1
--- funzione ordinamento 
---      1 quadrante \ring (x,y) -> y + (x - ring)
+-- distanceFromBase n = 
 
+precSquare n = go 1
+  where
+    go x
+      | (2*x+1) *  (2*x+1) > n = x-1
+      | otherwise = go (x + 1)
+
+{-
+
+:r
+fmap (translate (2,-1)) (orderedsquare 5)
+
+findBasePoint 1
+
+precSquare 9
+
+:t repeat
+:t P.zip
+
+-}
 
 main :: IO ()
 --main = interact $ pack . show . hardSol . parseInput
